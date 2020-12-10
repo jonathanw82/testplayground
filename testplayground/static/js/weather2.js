fd979cc04610c373707b3654b81acbc30e66147f
jonathanw82 <jonwhewaycode@outlook.com>
2020-12-10 12:30:00 +0000

    added javascript to flash the weather warning

--- testplayground/static/js/weather2.js
+++ testplayground/static/js/weather2.js
@@ -4,7 +4,49 @@ temp = document.getElementById('current_tempdisp').innerHTML;
 boxcolor = document.getElementById('weatherbox');
 separator = document.getElementById('separator');
 
-if (temp >= 20){
+if (temp >= 20) {
     boxcolor.classList.add('weatherboxhot');
     separator.classList.add('separatorhot');
+}
+
+// loosly taken from 
+// https://www.youtube.com/watch?v=q5g-hpYSppk
+// and https://leewc.com/articles/javascript-fade-in-out-callback/
+
+let weatherAlert = document.getElementById('alert_heading');
+let opacity = 0;
+let intervalID = 0;
+// if there is a westher alert
+if (weatherAlert) {
+    // get the opacity
+    opacity = Number(window.getComputedStyle(weatherAlert).getPropertyValue("opacity"));
+    s// start the show function
+    show();
+    function show() {
+        // set the interval to 100 miliseconds
+        intervalID = setInterval(function () {
+            if (opacity < 1) {
+                opacity = opacity + 0.1;
+                weatherAlert.style.opacity = opacity;
+            }
+            else {
+                // clear the timer
+                clearInterval(intervalID);
+                // start the next function
+                hide();
+            }
+        }, 100);
+    }
+    function hide() {
+        intervalID = setInterval(function () {
+            if (opacity > 0) {
+                opacity = opacity - 0.1;
+                weatherAlert.style.opacity = opacity;
+            }
+            else {
+                clearInterval(intervalID);
+                show();
+            }
+        }, 100);
+    }
 }
